e0e21f74b390c5d646c61dc960c096aae047dc6e
Latha-yata <lathayata1234@gmail.com>
2025-08-15 19:58:32 +0530

    Initial commit

---
+++ restaurent.js
@@ -0,0 +1,21 @@
+let mainimg=document.getElementById("mainimg");
+
+let img1var=document.getElementById('img1');
+img1var.addEventListener('click',()=>{
+    mainimg.setAttribute('src','./res photos/img12.jpg');
+})
+
+let img2var=document.getElementById('img2');
+img2var.addEventListener('click',()=>{
+    mainimg.setAttribute('src','./res photos/img14.jpg')
+})
+
+let img3var=document.getElementById('img3');
+img3var.addEventListener('click',()=>{
+    mainimg.setAttribute('src','./res photos/img21.jpg')
+})
+
+let img4var=document.getElementById('img4');
+img4var.addEventListener('click',()=>{
+    mainimg.setAttribute('src','./res photos/img31.jpg')
+})
